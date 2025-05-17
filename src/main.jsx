48c62348e3faca35e4eccb666bde4277ed43e1d0
Kirill Konovalov <auddax@gmail.com>
2025-05-17 16:28:23 +0300

    fix: wrap mapping (Sat, May 17, 2025 04:28:23 PM)

--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,24 @@ import { restaurants } from '../materials/mock';
 
 const root = createRoot(document.getElementById('root'));
 
-root.render(restaurants.map(({ id, name, menu, reviews }) => (
-  <section key={id}>
-    <h2>{name}</h2>
-    <h3>Меню</h3>
-    <ul>
-      {menu.map(({ id, name }) => (
-        <li key={id}>{name}</li>
-      ))}
-    </ul>
-    <h3>Отзывы</h3>
-    <ul>
-      {reviews.map(({ id, text }) => (
-        <li key={id}>{text}</li>
-      ))}
-    </ul>
-  </section>
-)));
+root.render(
+  <>
+    {restaurants.map(({ id, name, menu, reviews }) => (
+      <section key={id}>
+        <h2>{name}</h2>
+        <h3>Меню</h3>
+        <ul>
+          {menu.map(({ id, name }) => (
+            <li key={id}>{name}</li>
+          ))}
+        </ul>
+        <h3>Отзывы</h3>
+        <ul>
+          {reviews.map(({ id, text }) => (
+            <li key={id}>{text}</li>
+          ))}
+        </ul>
+      </section>
+    ))}
+  </>
+);
