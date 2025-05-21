6c7203b9f5d138fe01b94c86d414e9f0f7cc566d
Kirill Konovalov <auddax@gmail.com>
2025-05-21 12:52:05 +0300

    feat: add Restaurant component (Wed, May 21, 2025 12:52:05 PM)

---
+++ src/components/Restaurant/Restaurant.jsx
@@ -0,0 +1,16 @@
+import { Menu } from 'src/components/Menu/Menu';
+import { ReviewsList } from 'src/components/ReviewsList/ReviewsList';
+
+import styles from './Restaurant.module.scss';
+
+export const Restaurant = ({ restaurant = {} }) => {
+  const { name, menu, reviews } = restaurant;
+
+  return (
+    <section className={styles['restaurant']}>
+      <h2 className={styles['name']}>{name}</h2>
+      <Menu menu={menu} />
+      <ReviewsList reviews={reviews} />
+    </section>
+  );
+}
