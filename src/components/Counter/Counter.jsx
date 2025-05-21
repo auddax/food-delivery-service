164503f0f894ac51a45bca7fbf0e12083e64d29a
Kirill Konovalov <auddax@gmail.com>
2025-05-21 11:49:29 +0300

    feat: add Counter component (Wed, May 21, 2025 11:49:29 AM)

---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,17 @@
+import { MENU_ITEMS_COUNTER } from 'src/constants';
+import { useCounter } from 'src/hooks/useCounter'
+
+import styles from './Counter.module.scss';
+
+export const Counter = () => {
+  const { initialValue, minValue, maxValue} = MENU_ITEMS_COUNTER;
+  const [ count, increment, decrement] = useCounter(initialValue, minValue, maxValue);
+
+  return (
+    <span>
+      <button className={styles['counter-btn']} onClick={decrement}>-</button>
+      <span className={styles['count']}>{count}</span>
+      <button className={styles['counter-btn']} onClick={increment}>+</button>
+    </span>
+  );
+}
---
+++ src/constants/index.js
@@ -0,0 +1,5 @@
+export const MENU_ITEMS_COUNTER = {
+  initialValue: 0,
+  minValue: 0,
+  maxValue: 5,
+}
