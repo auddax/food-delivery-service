d5ed822e3102d59daa1f992c766dd3233787dcc6
Kirill Konovalov <auddax@gmail.com>
2025-05-23 18:28:07 +0300

    feat: add data check in Menu and ReviewsList components (Fri, May 23, 2025 06:28:07 PM)

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -3,13 +3,17 @@ import { Title } from 'src/components/Title/Title';
 
 import styles from './Menu.module.scss';
 
-export const Menu = ({ menu }) => (
-  <div className={styles['menu']}>
-    <Title level={3} value='Меню' />
-    <ul>
-      {menu?.map(({ id, ...restProps }) => (
-        <MenuItem key={id} {...restProps} />
-      ))}
-    </ul>
-  </div>
-);
+export const Menu = ({ menu }) => {
+  if (!menu?.length) return null;
+
+  return (
+    <div className={styles['menu']}>
+      <Title level={3} value='Меню' />
+      <ul>
+        {menu.map(({ id, ...restProps }) => (
+          <MenuItem key={id} {...restProps} />
+        ))}
+      </ul>
+    </div>
+  );
+};
--- src/components/ReviewsList/ReviewsList.jsx
+++ src/components/ReviewsList/ReviewsList.jsx
@@ -3,13 +3,17 @@ import { Title } from 'src/components/Title/Title';
 
 import styles from './ReviewsList.module.scss';
 
-export const ReviewsList = ({ reviews }) => (
-  <div className={styles['reviews-list']}>
-    <Title level={3} value='Отзывы' />
-    <ul>
-      {reviews?.map(({ id, ...restProps }) => (
-        <ReviewItem key={id} {...restProps} />
-      ))}
-    </ul>
-  </div>
-);
+export const ReviewsList = ({ reviews }) => {
+  if (!reviews?.length) return null;
+
+  return (
+    <div className={styles['reviews-list']}>
+      <Title level={3} value='Отзывы' />
+      <ul>
+        {reviews.map(({ id, ...restProps }) => (
+          <ReviewItem key={id} {...restProps} />
+        ))}
+      </ul>
+    </div>
+  );
+};
