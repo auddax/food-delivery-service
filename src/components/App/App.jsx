aede7bb5635f9c7cf9e3cec2e9aaeec854628705
Kirill Konovalov <auddax@gmail.com>
2025-05-29 17:46:23 +0300

    refactor: move ScrollProgressBar to Layout component (Thu, May 29, 2025 05:46:23 PM)

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,12 +2,10 @@ import './App.scss';
 
 import { Layout } from 'src/components/Layout/Layout';
 import { Restaurants } from 'src/components/Restaurants/Restaurants';
-import { ScrollProgressBar } from 'src/components/ScrollProgressBar/ScrollProgressBar';
 
 export const App = () => {
   return (
     <>
-      <ScrollProgressBar />
       <Layout>
         <Restaurants />
       </Layout>
