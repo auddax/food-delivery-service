b3bb5b71489fd0baacd7148d2981feb52edeb083
Kirill Konovalov <auddax@gmail.com>
2025-06-08 15:44:48 +0300

    feat: add CartButton component (Sun, Jun 08, 2025 03:44:48 PM)

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,11 @@
+import { CartButton } from 'src/components/CartButton/CartButton';
+
 import styles from './Footer.module.scss';
 
 export const Footer = () => (
   <section className={styles.footer}>
     <div>contact@example.com</div>
     <div>© {new Date().getFullYear()}</div>
+    <CartButton />
   </section>
 );
