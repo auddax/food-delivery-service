bc063a6d9434c9083903866738cb4675f82dd025
Kirill Konovalov <auddax@gmail.com>
2025-06-08 18:41:47 +0300

    feat: add cartSlice (Sun, Jun 08, 2025 06:41:47 PM)

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { cartSlice } from 'src/store/slices/cart/cart.slice';
 import { dishSlice } from 'src/store/slices/dish/dish.slice';
 import { restaurantSlice } from 'src/store/slices/restaurant/restaurant.slice';
 import { reviewSlice } from 'src/store/slices/review/review.slice';
@@ -6,6 +7,7 @@ import { userSlice } from 'src/store/slices/user/user.slice';
 
 export const store = configureStore({
   reducer: {
+    [cartSlice.name]: cartSlice.reducer,
     [dishSlice.name]: dishSlice.reducer,
     [restaurantSlice.name]: restaurantSlice.reducer,
     [reviewSlice.name]: reviewSlice.reducer,
